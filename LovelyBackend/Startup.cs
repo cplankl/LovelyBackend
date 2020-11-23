9038f2cb70c8c02be7e501763d46bcafddc9b282
Christian Plankl <chrplankl@live.de>
2020-11-23 19:01:24 +0100

    added pascal case

--- LovelyBackend/Startup.cs
+++ LovelyBackend/Startup.cs
@@ -39,7 +39,9 @@ namespace LovelyBackend
             });
 
 
-            services.AddControllers();
+            services.AddControllers()
+                .AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "LovelyBackend", Version = "v1" });
@@ -49,12 +51,10 @@ namespace LovelyBackend
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "LovelyBackend v1"));
-            }
+
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "LovelyBackend v1"));
 
             app.UseHttpsRedirection();
 
