25ed9086dee5cf69506a3f02981ebc41788912dd
Mona Lisa <mona@lisa.de>
2020-11-23 13:53:13 +0100

    added new controller

---
+++ LovelyBackend/Controllers/LovelyController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LovelyBackend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LovelyController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "from API with love";
+        }
+    }
+}
